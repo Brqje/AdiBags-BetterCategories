974ae75d5900abf89aeb0dfbfb4cef1a1203de2c
Baroque <33790616+Brqje@users.noreply.github.com>
2021-06-11 14:13:10 +0200

    Update Items.lua

--- src/data/Items.lua
+++ src/data/Items.lua
@@ -7,7 +7,6 @@ data.items = {
 -- Vanilla item list made by Baroque (EU-Magtheridon (Retail) / EU-Mograine (Classic))
 
 -- Everything in this file are items released in Vanilla only
-
 -- Classification according Vanilla rules
 ----------------------------------------------------------------------------------------
 
